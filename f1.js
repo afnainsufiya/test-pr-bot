56a10deaf02afb02a16714db3ad52af1f72d90fa
Afnain Sufiya <afnain.s@enervoir.in>
2026-05-19 09:30:50 +0530

    ch

--- f1.js
+++ f1.js
@@ -1,21 +1,5 @@
 // bad-code.js
-const mysql = require('mysql');
+const  badecode={
 
-function getUser(userId) {
-  const query = "SELECT * FROM users WHERE id = " + userId  // SQL injection!
-  db.query(query)
-
-  var password = "admin123"  // hardcoded secret!
-
-  if(true) {  // useless condition
-    console.log("user fetched")
-  }
 }
-
-function doEverything(a,b,c,d,e,f) {  // too many params!
-  for(var i=0;i<9999;i++) {  // performance issue!
-    for(var j=0;j<9999;j++) {
-      console.log(a+b+c+d+e+f)
-    }
-  }
-}
+function add(a,b){
