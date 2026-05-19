f1b6018ecbb44969318a03be7849b3a85dde2b36
Afnain Sufiya <afnain.s@enervoir.in>
2026-05-19 13:01:14 +0530

    v

--- bad-code.js
+++ bad-code.js
@@ -19,10 +19,3 @@ function doEverything(a,b,c,d,e,f) {  // too many params!
     }
   }
 }
-function doEverything(a,b,c,d,e,f,t,t,t,t,t,t,) {  // too many params!
-  for(var i=0;i<9999;i++) {  // performance issue!
-    for(var j=0;j<9999;j++) {
-      console.log(a+b+c+d+e+f)
-    }
-  }
-}
