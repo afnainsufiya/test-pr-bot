7ba397200de8645923ff6585256758783bc935a3
Afnain Sufiya <afnain.s@enervoir.in>
2026-05-19 10:14:59 +0530

    v

---
+++ b1.js
@@ -0,0 +1,10 @@
+function getUser(userId) {
+  const query = "SELECT * FROM users WHERE id = " + userId  // SQL injection!
+  db.query(query)
+
+  var password = "admin123"  // hardcoded secret!
+
+  if(true) {  // useless condition
+    console.log("user fetched")
+  }
+}
