bacf19f1e354a06b1069f1f8f18a70ffea53979d
afnainsufiya <143891167+afnainsufiya@users.noreply.github.com>
2026-05-26 11:28:56 +0530

    changes

--- f2.js
+++ f2.js
@@ -5,7 +5,7 @@ function getUser(userId) {
   const query = "SELECT * FROM users WHERE id = " + userId  // SQL injection!
   db.query(query)
 
-  var password = "admin123"  // hardcoded secret!         
+  var password = "admin123"  // hardcoded secret!
 
   if(true) {  // useless condition
     console.log("user fetched")
@@ -13,13 +13,6 @@ function getUser(userId) {
 }
 
 function doEverything(a,b,c,d,e,f) {  // too many params!
-  for(var i=0;i<9999;i++) {  // performance issue!
-    for(var j=0;j<9999;j++) {
-      console.log(a+b+c+d+e+f)
-    }
-  }
-}
-function ww33(a,b,c,d,e,f) {  // too many params!
   for(var i=0;i<9999;i++) {  // performance issue!
     for(var j=0;j<9999;j++) {
       console.log(a+b+c+d+e+f)
